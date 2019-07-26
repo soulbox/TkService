0003f32331e5f141ac7207c9e3a539459b555779
kadir aygün <kadir2008@hotmail.com>
2019-07-26 16:53:59 +0300

    Login view

---
+++ TekService/TekService/TekService/Data/imageConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TekService.Data
+{
+    using System.Globalization;
+    using Xamarin.Forms;
+
+    class imageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ImageSource.FromResource("TekService.images." + (value ?? ""));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TekService/TekService/TekService/Views/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace TekService.Views
+{
+    using static  Data.imageConverter;
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+        Data.imageConverter imageConverter = new Data.imageConverter();
+        public Login ()
+		{
+            InitializeComponent ();
+            logo.BindingContext = "logo2.png";
+            
+		}
+
+        private void SwHatırla_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
